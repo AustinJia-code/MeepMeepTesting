c343be4064c3825d392849be55d4e90eb5f66446
Scurry200 <sherwinamal1@gmail.com>
2022-02-05 10:56:31 -0600

    redleft and redright simulations?

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -13,13 +13,18 @@ public class MeepMeepTesting {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(50, 30, Math.toRadians(180), Math.toRadians(180), 18.5)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 90))
-                                .splineTo(new Vector2d(6, 20), Math.toRadians(0))
-                                .turn(Math.toRadians(-140))
-                                .turn(Math.toRadians(-40))
-                                .splineTo(new Vector2d(-25, 6), Math.toRadians(-180))
-                                .lineTo(new Vector2d(-16, 12))
+                        drive.trajectorySequenceBuilder(new Pose2d(-36, -70, 90))
+                                .splineTo(new Vector2d(-24, -25.5), Math.toRadians(90))
+                                .turn(Math.toRadians(100))
+                                .turn(Math.toRadians(80))
+                                .splineTo(new Vector2d(-60, -65), Math.toRadians(-180))
+                                .lineTo(new Vector2d(-52, -55))
                                 .turn(Math.toRadians(90))
+                                .lineTo(new Vector2d(-50,-68)) //assuming where duck is
+                                .setReversed(true)
+                                .splineTo(new Vector2d(-24,-24),Math.toRadians(0))
+                                .lineTo(new Vector2d(-61,-35))
+
 
                                 .build()
                 );
@@ -27,16 +32,17 @@ public class MeepMeepTesting {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(50, 30, Math.toRadians(180), Math.toRadians(180), 18.5)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 90))
-                                .splineTo(new Vector2d(-6, 20), Math.toRadians(0))
-                                .turn(Math.toRadians(-45))
-                                .splineTo(new Vector2d(45,4),Math.toRadians(0))
+                        drive.trajectorySequenceBuilder(new Pose2d(11, -70, 90))
+                                .splineTo(new Vector2d(-4, -31), Math.toRadians(90))
+
+                                .turn(Math.toRadians(-135))
+                                .splineTo(new Vector2d(55,-63),Math.toRadians(0))
                                 .setReversed(true)
-                                .splineTo(new Vector2d(-6,20),Math.toRadians(-225))
+                                .splineTo(new Vector2d(-4,-31),Math.toRadians(-225))
                                 .setReversed(false)
-                                .splineTo(new Vector2d(45,4),Math.toRadians(0))
+                                .splineTo(new Vector2d(55,-63),Math.toRadians(0))
                                 .setReversed(true)
-                                .splineTo(new Vector2d(-6,20),Math.toRadians(-225))
+                                .splineTo(new Vector2d(-4,-31),Math.toRadians(-225))
 
 
 
