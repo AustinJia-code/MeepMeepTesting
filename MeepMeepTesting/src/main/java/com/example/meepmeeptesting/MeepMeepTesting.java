a2ccc30feee6339e75e3c5d46762a5c51851a4a5
Scurry200 <sherwinamal1@gmail.com>
2022-02-04 22:45:31 -0600

    meepmeep redleft testing?

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -0,0 +1,73 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class MeepMeepTesting {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+        RoadRunnerBotEntity redLeft = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 90))
+                                .splineToLinearHeading(new Pose2d(6, 20), Math.toRadians(0))
+                                .turn(Math.toRadians(-140))
+                                .turn(Math.toRadians(-40))
+                                .splineTo(new Vector2d(-25, 6), Math.toRadians(-180))
+                                .lineTo(new Vector2d(-16, 12))
+                                .turn(Math.toRadians(90))
+
+                                .build()
+                );
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 90))
+                                .splineToLinearHeading(new Pose2d(6, 20), Math.toRadians(0))
+                                .turn(Math.toRadians(-140))
+                                .turn(Math.toRadians(-40))
+
+
+
+
+                                .build()
+                );
+        RoadRunnerBotEntity myBot1 = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(6, 20, -180))
+
+
+
+                                .splineTo(new Vector2d(-25, 6), Math.toRadians(-180))
+
+                                .build()
+                );
+        RoadRunnerBotEntity myBot2 = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(-21, 6, Math.toRadians(-180)))
+
+
+
+                                .lineTo(new Vector2d(-16, 12))
+                                .turn(Math.toRadians(90))
+
+                                .build()
+                );
+
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_FREIGHTFRENZY_ADI_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(redLeft)
+                .start();
+    }
+}
