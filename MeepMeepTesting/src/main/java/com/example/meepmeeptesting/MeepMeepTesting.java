7a5b01361804069e65f0e6789f10e4e293850c07
Austin Jia <56139100+AustinJia-code@users.noreply.github.com>
2022-11-22 22:21:59 -0600

    mid

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -13,7 +13,81 @@ public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(600);
 
-        RoadRunnerBotEntity traj10 = new DefaultBotBuilder(meepMeep)
+        RoadRunnerBotEntity traj = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(45, 35, 5,4, 13)
+                .setDimensions(13.44, 14.8)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(35, -62.5, Math.toRadians(-90)))
+
+                                //score preload
+                                .back(10)
+                                .splineTo(new Vector2d(32,-24), Math.toRadians(180))
+
+                                //go to storage
+                                .setReversed(false)
+                                .splineTo(new Vector2d(48,-12), Math.toRadians(0))
+                                .forward(12)
+
+                                //go to pole
+                                .setReversed(true)
+                                .splineTo(new Vector2d(30,-18), Math.toRadians(-140))
+
+                                //go to storage
+                                .setReversed(false)
+                                .splineTo(new Vector2d(48,-12), Math.toRadians(0))
+                                .forward(12)
+
+                                //go to pole
+                                .setReversed(true)
+                                .splineTo(new Vector2d(30,-18), Math.toRadians(-140))
+
+                                //go to storage
+                                .setReversed(false)
+                                .splineTo(new Vector2d(48,-12), Math.toRadians(0))
+                                .forward(12)
+
+                                //go to pole
+                                .setReversed(true)
+                                .splineTo(new Vector2d(30,-18), Math.toRadians(-140))
+
+                                //go to storage
+                                .setReversed(false)
+                                .splineTo(new Vector2d(48,-12), Math.toRadians(0))
+                                .forward(12)
+
+                                //go to pole
+                                .setReversed(true)
+                                .splineTo(new Vector2d(30,-18), Math.toRadians(-140))
+
+                                //go to storage
+                                .setReversed(false)
+                                .splineTo(new Vector2d(48,-12), Math.toRadians(0))
+                                .forward(12)
+
+                                //go to pole
+                                .setReversed(true)
+                                .splineTo(new Vector2d(30,-18), Math.toRadians(-140))
+
+                                .setReversed(false)
+                                .splineTo(new Vector2d(35, -35), Math.toRadians(-90))
+
+
+
+                                //build
+                                .build());
+
+    meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+               // .addEntity(redRightSafeTSE2)
+           .addEntity(traj)
+            .start();
+    }
+}
+
+/*
+        RoadRunnerBotEntity traj = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(45, 35, 5,4, 13)
                 .setDimensions(13.44, 14.8)
@@ -36,16 +110,11 @@ public class MeepMeepTesting {
 
                                 .waitSeconds(1)
                                 .setReversed(false)
-                                .splineTo(new Vector2d(35, -35), Math.toRadians(-90))
+                                .splineTo(new Vector2d(35, -20), Math.toRadians(-90))
+                                //.forward(20)
+                                .splineTo(new Vector2d(11, -35), Math.toRadians(-90))
 
                                 //build
                                 .build());
 
-    meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
-                .setDarkMode(true)
-                .setBackgroundAlpha(0.95f)
-               // .addEntity(redRightSafeTSE2)
-           .addEntity(traj10)
-            .start();
-    }
-}
+         */
