2de760c4a8f9e819e2e6b11d32098709c55fe10a
Austin Jia <56139100+AustinJia-code@users.noreply.github.com>
2023-01-22 21:23:04 -0600

    RAAAAAH

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepMid.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepMid.java
@@ -19,26 +19,65 @@ public class MeepMeepMid {
                 .setConstraints(45, 35, 5,4, 13)
                 .setDimensions(13.44, 14.8)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(35, -62.5, Math.toRadians(-90)))
+                        drive.trajectorySequenceBuilder(new Pose2d(35.5, -62.5, Math.toRadians(-90)))
 
                                 //score preload
-                                .setReversed(true)
-                                .splineTo(new Vector2d(32,-40), Math.toRadians(135))
-                                .splineTo(new Vector2d(5, -30), Math.toRadians(135))
-                                //.splineTo(new Vector2d(32,-24), Math.toRadians(180))
+                                .back(39)
+                                .splineTo(new Vector2d(6.5,-18.75), Math.toRadians(215))
 
-                                //go to storage
+                                //go to storage + 1
                                 .setReversed(false)
-                                .splineTo(new Vector2d(27,-38), Math.toRadians(0))
-                                .splineTo(new Vector2d(48, -12), Math.toRadians(0))
-                                .forward(12)
+                                .splineTo(new Vector2d(35,-12), Math.toRadians(0))
+                                .forward(26)
 
-                                //go to pole
+                                //go to pole + 1
                                 .setReversed(true)
-                                .splineTo(new Vector2d(30,-18), Math.toRadians(-140))
+                                .back(11)
+                                .splineTo(new Vector2d(30,-18), Math.toRadians(225))
 
+                                //go to storage + 2
                                 .setReversed(false)
-                                .lineToLinearHeading(new Pose2d(35, -12, Math.toRadians(-90)))
+                                .splineTo(new Vector2d(50,-12), Math.toRadians(0))
+                                .forward(11)
+
+                                //go to pole + 2
+                                .setReversed(true)
+                                .back(11)
+                                .splineTo(new Vector2d(30,-18), Math.toRadians(225))
+
+                                //go to storage + 3
+                                .setReversed(false)
+                                .splineTo(new Vector2d(50,-12), Math.toRadians(0))
+                                .forward(11)
+
+                                //go to pole + 3
+                                .setReversed(true)
+                                .back(11)
+                                .splineTo(new Vector2d(30,-18), Math.toRadians(225))
+
+                                //go to storage + 4
+                                .setReversed(false)
+                                .splineTo(new Vector2d(50,-12), Math.toRadians(0))
+                                .forward(11)
+
+                                //go to pole + 4
+                                .setReversed(true)
+                                .back(11)
+                                .splineTo(new Vector2d(30,-18), Math.toRadians(225))
+
+                                //go to storage + 5
+                                .setReversed(false)
+                                .splineTo(new Vector2d(50,-12), Math.toRadians(0))
+                                .forward(11)
+
+                                //go to pole + 5
+                                .setReversed(true)
+                                .back(11)
+                                .splineTo(new Vector2d(30,-18), Math.toRadians(225))
+
+                                //park
+                                .setReversed(false)
+                                .lineToLinearHeading(new Pose2d(36, -12, Math.toRadians(-90)))
 
                                 //build
                                 .build());
@@ -48,24 +87,65 @@ public class MeepMeepMid {
                 .setConstraints(45, 35, 5,4, 13)
                 .setDimensions(13.44, 14.8)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(-35, -62.5, Math.toRadians(-90)))
+                        drive.trajectorySequenceBuilder(new Pose2d(-35.5, -62.5, Math.toRadians(-90)))
 
                                 //score preload
-                                .setReversed(true)
-                                .lineToLinearHeading(new Pose2d(-31,-29.5, Math.toRadians(-135)))
-                                //.back(4)
+                                .back(39)
+                                .splineTo(new Vector2d(-6.5,-18.75), Math.toRadians(-35))
 
-                                //go to storage
-                                .setReversed(true)
-                                .lineToLinearHeading(new Pose2d(-41,-12, Math.toRadians(180)))
-                                .forward(20)
-
-                                //go to pole
-                                .setReversed(true)
-                                .splineTo(new Vector2d(-30,-18), Math.toRadians(-40))
-
+                                //go to storage + 1
                                 .setReversed(false)
-                                .lineToLinearHeading(new Pose2d(-35, -11, Math.toRadians(-90)))
+                                .splineTo(new Vector2d(-35,-12), Math.toRadians(180))
+                                .forward(26)
+
+                                //go to pole + 1
+                                .setReversed(true)
+                                .back(11)
+                                .splineTo(new Vector2d(-30,-18), Math.toRadians(-45))
+
+                                //go to storage + 2
+                                .setReversed(false)
+                                .splineTo(new Vector2d(-50,-12), Math.toRadians(180))
+                                .forward(11)
+
+                                //go to pole + 2
+                                .setReversed(true)
+                                .back(11)
+                                .splineTo(new Vector2d(-30,-18), Math.toRadians(-45))
+
+                                //go to storage + 3
+                                .setReversed(false)
+                                .splineTo(new Vector2d(-50,-12), Math.toRadians(180))
+                                .forward(11)
+
+                                //go to pole + 3
+                                .setReversed(true)
+                                .back(11)
+                                .splineTo(new Vector2d(-30,-18), Math.toRadians(-45))
+
+                                //go to storage + 4
+                                .setReversed(false)
+                                .splineTo(new Vector2d(-50,-12), Math.toRadians(180))
+                                .forward(11)
+
+                                //go to pole + 4
+                                .setReversed(true)
+                                .back(11)
+                                .splineTo(new Vector2d(-30,-18), Math.toRadians(-45))
+
+                                //go to storage + 5
+                                .setReversed(false)
+                                .splineTo(new Vector2d(-50,-12), Math.toRadians(180))
+                                .forward(11)
+
+                                //go to pole + 5
+                                .setReversed(true)
+                                .back(11)
+                                .splineTo(new Vector2d(-30,-18), Math.toRadians(-45))
+
+                                //park
+                                .setReversed(false)
+                                .lineToLinearHeading(new Pose2d(-36, -12, Math.toRadians(-90)))
 
                                 //build
                                 .build());
